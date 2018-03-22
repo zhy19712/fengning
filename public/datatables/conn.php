607c5f3e0a05cf311b7293fd01bd3acbe88f8f73
zhy19712 <18055089@qq.com>
2018-03-22 15:53:14 +0800

    datatables added

--- public/datatables/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-// SQL server connection information数据库连接信息
-global $sql_details;
-$sql_details = array(
-    'user' => 'root',
-    'pass' => 'admin',
-    'db'   => 'fengning',
-    'host' => '127.0.0.1'
-);
