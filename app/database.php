2da623674aac964f161cafb954ed3cadc3f5a4e7
zhy19712 <18055089@qq.com>
2018-03-22 18:15:30 +0800

    datatables added

--- app/database.php
+++ app/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '192.168.1.2',
     // 数据库名
     'database'        => 'fengning',
     // 用户名
