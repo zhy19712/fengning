af7a500a1c6f0a9202559678fefaf7dba02c675f
祈晴宝宝 <373884384@qq.com>
2018-03-23 15:21:05 +0800

    合同登记

--- app/contract/controller/Contract.php
+++ app/contract/controller/Contract.php
@@ -9,6 +9,9 @@
 namespace app\contract\controller;
 
 use app\admin\controller\Permissions;
+use app\contract\model\ContractModel;
+use think\Exception;
+use think\Request;
 
 class Contract extends Permissions
 
@@ -16,5 +19,45 @@ class Contract extends Permissions
     public function index()
     {
         return $this->fetch();
+    }
+
+    /**
+     * 新增或修改
+     * @return array
+     */
+    public function addoredit()
+    {
+        if ($this->request->isAjax()) {
+            try {
+                $mod = input('post');
+                $m = new ContractModel();
+                $res = $m->AddOrEdit($mod);
+                if ($res) {
+                    return ['code' => 1];
+                } else {
+                    return ['code' => -1];
+                }
+            } catch (Exception $e) {
+                return ['code' => -1, 'msg' => $e->getMessage()];
+            }
+        }
+    }
+
+    public function del()
+    {
+        if ($this->request->isAjax()) {
+            try {
+                $id = input('id');
+                $res = ContractModel::destroy($id);
+                if ($res) {
+                    return ['code' => 1];
+                } else {
+                    return ['code' => -1];
+                }
+            } catch (Exception $e) {
+                return ['code' => -1, 'msg' => $e->getMessage()];
+            }
+        }
+
     }
 }
