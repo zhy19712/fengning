b32e81c885ab22e79bd6bc0ac7171358cb06de47
13522390376 <13522390376@163.com>
2018-03-26 14:42:10 +0800

    角色分类树

--- app/admin/model/AdminCateType.php
+++ app/admin/model/AdminCateType.php
@@ -9,6 +9,7 @@ namespace app\admin\model;
 
 use think\Model;
 use think\Session;
+use think\exception\PDOException;
 
 class AdminCateType extends Model
 {
@@ -20,5 +21,38 @@ class AdminCateType extends Model
     public function getall()
     {
         return $this->select();
+    }
+
+    /*
+     * 添加角色类型节点
+     */
+    public function insertCateType($param)
+    {
+        try{
+            $result = $this->allowField(true)->save($param);
+            if(false === $result){
+                return ['code' => -1,'msg' => $this->getError()];
+            }else{
+                return ['code' => 1,'msg' => '添加成功'];
+            }
+        }catch (PDOException $e){
+            return ['code' => -1,'msg' => $e->getMessage()];
+        }
+    }
+    /*
+     * 编辑角色类型节点
+     */
+    public function editTb($param)
+    {
+        try{
+            $result = $this->allowField(true)->save($param,['id' => $param['id']]);
+            if(false === $result){
+                return ['code' => -1,'msg' => $this->getError()];
+            }else{
+                return ['code' => 1, 'msg' => '编辑成功'];
+            }
+        }catch(PDOException $e){
+            return ['code' => 0, 'msg' => $e->getMessage()];
+        }
     }
 }
