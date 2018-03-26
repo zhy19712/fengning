f4aa17abe6053f6cf49a3923d1c8b9f0ee44ae3a
13522390376 <13522390376@163.com>
2018-03-26 16:25:42 +0800

    角色分类树

--- app/admin/model/Admin.php
+++ app/admin/model/Admin.php
@@ -46,4 +46,31 @@ class Admin extends Model
         return ['code' => 1, 'msg' => '删除成功'];
     }
 
+    /**
+     * 根据角色类型管理 编号 关联删除 用户
+     * @param $group_id
+     * @return array
+     * @author hutao
+     */
+    public function delUserByCateId($cate_id)
+    {
+        $user = $this->where('admin_cate_id',$cate_id)->column('id','thumb');
+        if(count($user) > 0){
+            $thumbArr = array_values($user);
+            $thumbPath = Db::name('attachment')->whereIn($thumbArr)->column('filepath');
+            if(count($thumbPath) > 0){
+                // 删除每一个用户的头像
+                foreach($thumbPath as $k=>$v){
+                    if(file_exists($v)){
+                        unlink($v); //删除文件
+                    }
+                }
+            }
+            // 删除用户记录
+            $idArr = array_keys($user);
+            $this->delete($idArr);
+        }
+        return ['code' => 1, 'msg' => '删除成功'];
+    }
+
 }
