357d80cf4ae52593b52d3e55dce5d549c146219a
祈晴宝宝 <373884384@qq.com>
2018-03-27 16:26:41 +0800

    单位质量

--- app/quality/controller/Unit.php
+++ app/quality/controller/Unit.php
@@ -3,18 +3,14 @@
  * Created by PhpStorm.
  * User: zhifang
  * Date: 2018/3/27
- * Time: 16:16
+ * Time: 16:24
  */
-
 namespace app\quality\controller;
-
 use app\admin\controller\Permissions;
 
-/**
- * 工程划分
- * Class Division
- * @package app\quality\controller
- */
-class Division extends Permissions{
-
+class Unit extends Permissions{
+    public  function index()
+    {
+        return $this->fetch();
+    }
 }
