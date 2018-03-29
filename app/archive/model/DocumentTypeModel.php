42dac1159eecf02bad0dd22ba8b2fd7f08ad928f
祈晴宝宝 <373884384@qq.com>
2018-03-29 09:34:37 +0800

    图纸文档管理

---
+++ app/archive/model/DocumentTypeModel.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhifang
+ * Date: 2018/3/29
+ * Time: 9:32
+ */
+namespace app\archive\model;
+use think\Model;
+
+class DocumentTypeModel extends Model{
+    protected $name='archive_document_type';
+}
