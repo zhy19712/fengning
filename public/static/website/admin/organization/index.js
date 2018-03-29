733868103c811bbe7347797485e259ef3ae7a4b2
wyang <wyang1117@msn.com>
2018-03-29 18:57:06 +0800

    完成个人中心页面

--- public/static/website/admin/organization/index.js
+++ public/static/website/admin/organization/index.js
@@ -415,6 +415,7 @@
         admin_group_name = treeNode.name;
 
         $('#tableItem_wrapper').show();
+        $('.tbcontainer').show();
         tableItem.ajax.url("/admin/common/datatablesPre?tableName=admin&id="+ treeNode.id).load();
         console.log(tableItem.rows().count());
 
@@ -435,71 +436,76 @@
     zTreeObj = $.fn.zTree.init($("#ztree"), setting, null);
 
     //组织结构表格
-       var tableItem = $('#tableItem').DataTable( {
-           processing: true,
-           serverSide: true,
-           ajax: {
-               "url":"/admin/common/datatablesPre?tableName=admin"
+   var tableItem = $('#tableItem').DataTable( {
+       processing: true,
+       serverSide: true,
+       ajax: {
+           "url":"/admin/common/datatablesPre?tableName=admin"
+       },
+       columns: [
+           {
+               name: "g_order"
            },
-           columns: [
-               {
-                   name: "g_order"
-               },
-               {
-                   name: "g_name"
-               },
-               {
-                   name: "nickname"
-               },
-               {
-                   name: "name"
-               },
-               {
-                   name: "mobile"
-               },
-               {
-                   name: "position"
-               },
-               {
-                   name: "status"
-               },
-               {
-                   name: "id"
-               }
-           ],
-           columnDefs: [
-               {
-                   "searchable": false,
-                   "orderable": false,
-                   "targets": [7],
-                   "render" :  function(data,type,row) {
-                       var status = row[6];    //后台返回的是否禁用用户状态
-                       var html = "<i class='fa fa-search' uid="+ data +" title='查看' onclick='view(this)'></i>" ;
-                       html += "<i class='fa fa-pencil' uid="+ data +" title='编辑' onclick='editor(this)'></i>" ;
-                       html += "<i class='fa fa-cog' uid="+ data +" title='重置密码' onclick='reset(this)'></i>" ;
-                       if(status==1){
-                           html += "<i class='fa fa-user-secret' uid="+ data +" status="+ status +" title='置为无效' onclick='audit(this)'></i>" ;
-                       }else if(status==0){
-                           html += "<i class='fa fa-user-times' uid="+ data +" status="+ status +" title='置为有效' onclick='audit(this)'></i>" ;
-                       }
-                       return html;
+           {
+               name: "g_name"
+           },
+           {
+               name: "nickname"
+           },
+           {
+               name: "name"
+           },
+           {
+               name: "mobile"
+           },
+           {
+               name: "position"
+           },
+           {
+               name: "status"
+           },
+           {
+               name: "id"
+           }
+       ],
+       columnDefs: [
+           {
+               "searchable": false,
+               "orderable": false,
+               "targets": [7],
+               "render" :  function(data,type,row) {
+                   var status = row[6];    //后台返回的是否禁用用户状态
+                   var html = "<i class='fa fa-search' uid="+ data +" title='查看' onclick='view(this)'></i>" ;
+                   html += "<i class='fa fa-pencil' uid="+ data +" title='编辑' onclick='editor(this)'></i>" ;
+                   html += "<i class='fa fa-cog' uid="+ data +" title='重置密码' onclick='reset(this)'></i>" ;
+                   if(status==1){
+                       html += "<i class='fa fa-user-secret' uid="+ data +" status="+ status +" title='置为无效' onclick='audit(this)'></i>" ;
+                   }else if(status==0){
+                       html += "<i class='fa fa-user-times' uid="+ data +" status="+ status +" title='置为有效' onclick='audit(this)'></i>" ;
                    }
-               }
-           ],
-           language: {
-               "sProcessing":"数据加载中...",
-               "lengthMenu": "每页_MENU_ 条记录",
-               "zeroRecords": "没有找到记录",
-               "info": "第 _PAGE_ 页 ( 总共 _PAGES_ 页 )",
-               "infoEmpty": "无记录",
-               "search": "搜索：",
-               "infoFiltered": "(从 _MAX_ 条记录过滤)",
-               "paginate": {
-                   "previous": "上一页",
-                   "next": "下一页"
+                   return html;
                }
            }
-       });
+       ],
+       language: {
+           "sProcessing":"数据加载中...",
+           "lengthMenu": "每页_MENU_ 条记录",
+           "zeroRecords": "没有找到记录",
+           "info": "第 _PAGE_ 页 ( 总共 _PAGES_ 页 )",
+           "infoEmpty": "无记录",
+           "search": "搜索：",
+           "infoFiltered": "(从 _MAX_ 条记录过滤)",
+           "paginate": {
+               "previous": "上一页",
+               "next": "下一页"
+           }
+       },
+       fnInitComplete: function (oSettings, json) {
+           $('#tableItem_length').insertBefore(".mark");
+           $('#tableItem_info').insertBefore(".mark");
+           $('#tableItem_paginate').insertBefore(".mark");
+       }
+   });
 
     //新增上传电子签名
     uploader = WebUploader.create({
@@ -805,6 +811,7 @@
                 $('select[name="gender"]').val(data.gender);
                 $('input[name="signature"]').val(data.signature);
                 $('textarea[name="remark"]').val(data.remark);
+                initUi.form.render('select');
             }
         });
     }
