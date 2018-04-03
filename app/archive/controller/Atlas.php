561d2826d214a527186435560f893d9d4e0415a3
13522390376 <13522390376@163.com>
2018-04-03 09:52:32 +0800

    角色分类树

--- app/archive/controller/Atlas.php
+++ app/archive/controller/Atlas.php
@@ -330,7 +330,6 @@ class Atlas extends Permissions
     public function atlascateDownload()
     {
         $id = input('param.id');
-
         $model = new AtlasCateModel();
         $download = new AtlasDownloadModel();
         $param = $model->getOne($id);
@@ -420,54 +419,46 @@ class Atlas extends Permissions
     }
 
 
-        //获取文件列表
-        function list_dir($dir){
-            $result = array();
-            if (is_dir($dir)){
-                $file_dir = scandir($dir);
-                foreach($file_dir as $file){
-                    if ($file == '.' || $file == '..'){
-                        continue;
-                    }
-                    elseif (is_dir($dir.$file)){
-                        $result = array_merge($result, list_dir($dir.$file.'/'));
-                    }
-                    else{
-                        array_push($result, $dir.$file);
-                    }
-                }
-            }
-            return $result;
-        }
-    public function  download()
+    /**
+     * 图册打包下载
+     */
+    public function allDownload()
     {
+        //获取列表
+        $param = input('post.');
+//        $id = $param['id'];//图册的id
 
-//获取列表
-        $datalist=list_dir('../');
-        $filename = "./bak.zip"; //最终生成的文件名（含路径）
-        if(!file_exists($filename)){
-//重新生成文件
-            $zip = new ZipArchive();//使用本类，linux需开启zlib，windows需取消php_zip.dll前的注释
-            if ($zip->open($filename, ZIPARCHIVE::CREATE)!==TRUE) {
-                exit('无法打开文件，或者文件创建失败');
-            }
-            foreach( $datalist as $val){
-                if(file_exists($val)){
-                    $zip->addFile( $val, basename($val));//第二个参数是放在压缩包中的文件名称，如果文件可能会有重复，就需要注意一下
-                }
-            }
-            $zip->close();//关闭
+        $model = new AtlasCateModel();
+
+        //定义一个空的数组
+
+
+        $datalist = $model->getallpath($param['id']);
+
+        $zip = new \ZipArchive;
+//压缩文件名
+        $filename = 'download.zip';
+//新建zip压缩包
+        $zip->open($filename,\ZipArchive::OVERWRITE|\ZipArchive::CREATE);
+//把图片一张一张加进去压缩
+        foreach ($datalist as $key => $value) {
+
+            $zip->addFile($value);
         }
-        if(!file_exists($filename)){
-            exit("无法找到文件"); //即使创建，仍有可能失败。。。。
-        }
+//打包zip
+        $zip->close();
+
+//可以直接重定向下载
+        header('Location:'.$filename);
+
+//或者输出下载
         header("Cache-Control: public");
         header("Content-Description: File Transfer");
         header('Content-disposition: attachment; filename='.basename($filename)); //文件名
-        header("Content-Type: application/zip"); //zip格式的
-        header("Content-Transfer-Encoding: binary"); //告诉浏览器，这是二进制文件
+        header("Content-Type: application/force-download");
+        header("Content-Transfer-Encoding: binary");
         header('Content-Length: '. filesize($filename)); //告诉浏览器，文件大小
-        @readfile($filename);
+        readfile($filename);
     }
 
 
