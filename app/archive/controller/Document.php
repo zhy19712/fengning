286b58730591c923ecf63c5016063ed9d3a05fdf
zhifang <373884384@qq.com>
2018-04-03 17:42:01 +0800

    文档权限编辑

--- app/archive/controller/Document.php
+++ app/archive/controller/Document.php
@@ -222,12 +222,11 @@ class Document extends Permissions
                 } else {
                     return json(['code' => 1, 'msg' => "", 'data' => $path]);
                 }
-            }else
-            {
-                return json(['code'=>-1,'msg'=>'没有权限']);
+            } else {
+                return json(['code' => -1, 'msg' => '没有权限']);
             }
         }
-        $this->assign('url',$url);
+        $this->assign('url', $url);
         return $this->fetch();
     }
 
@@ -249,14 +248,15 @@ class Document extends Permissions
             } else {
                 return json(['code' => -1]);
             }
+        } else {
+            //显示文档权限用户
+            $doc = DocumentModel::get($id);
+            if (empty($doc['users'])) {
+                return json();
+            }
+            $users = explode("|", $doc['users']);
+            $list = Db::table('admin')->whereIn('id', $users)->field('id,nickname')->select();
+            return json($list);
         }
-        //显示文档权限用户
-        $doc = DocumentModel::get($id);
-        if (empty($doc['users'])) {
-            return json();
-        }
-        $users = explode("|", $doc['users']);
-        $list = Db::table('admin')->whereIn('id', $users)->field('id,nickname')->select();
-        return json($list);
     }
 }
