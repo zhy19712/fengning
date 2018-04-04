641992fc098316a85cff5d7af16449ad4ab4b879
13522390376 <13522390376@163.com>
2018-04-04 14:32:52 +0800

    角色分类树

--- app/archive/controller/Common.php
+++ app/archive/controller/Common.php
@@ -231,7 +231,7 @@ class Common extends Controller
         $recordsTotal = Db::name($table)->where('cate_id',$id)->count(0);
         $recordsFilteredResult = array();
             //没有搜索条件的情况
-                $recordsFilteredResult = Db::name($table)->where('cate_id',$id)->limit(intval($start), intval($length))->select();
+                $recordsFilteredResult = Db::name($table)->where('cate_id',$id)->order("date desc")->limit(intval($start), intval($length))->select();
 
                 //*****多表查询join改这里******
                 //$recordsFilteredResult = Db::name('datatables_example')->alias('d')->join('datatables_example_join e','d.position = e.id')->field('d.id,d.name,e.name as position,d.office')->select();
