08becb1892267b3c29eb87acf75d3a616eda0fe5
zhifang <373884384@qq.com>
2018-04-08 09:54:54 +0800

    模板管理

--- app/standard/controller/Templatemanage.php
+++ app/standard/controller/Templatemanage.php
@@ -9,16 +9,54 @@
 namespace app\standard\controller;
 
 use app\admin\controller\Permissions;
+use app\standard\model\TemplateModel;
+use think\Request;
 
 class Templatemanage extends Permissions
 {
+    protected $templateService;
+
+    public function __construct(Request $request = null)
+    {
+        parent::__construct($request);
+        $this->templateService = new TemplateModel();
+    }
+
     public function index()
     {
         return $this->fetch();
     }
 
+    /**
+     * 添加编辑模板
+     * @param null $id
+     * @return mixed|\think\response\Json
+     */
     public function add($id = null)
     {
+        if ($this->request->isAjax()) {
+            $mod = input('post.');
+            if (empty($mod['id'])) {
+                $res = $this->templateService->allowField(true)->save($mod);
+            }
+            $res = $this->templateService->allowField(true)->save($mod, ['id' => $mod['id']]);
+            if ($res) {
+                return json(['code' => 1]);
+            } else {
+                return json(['code' => -1]);
+            }
+        }
         return $this->fetch();
+    }
+
+    /**
+     * 获取一条记录
+     * @param $id
+     * @return \think\response\Json
+     * @throws \think\exception\DbException
+     */
+    public function getone($id)
+    {
+        return json( TemplateModel::get($id));
     }
 }
