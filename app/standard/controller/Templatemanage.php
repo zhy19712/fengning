0d6bdb009bc08b1ffba80f4636e1c0dc8da37a37
zhifang <373884384@qq.com>
2018-04-08 09:20:07 +0800

    模板管理

--- app/standard/controller/Templatemanage.php
+++ app/standard/controller/Templatemanage.php
@@ -5,12 +5,19 @@
  * Date: 2018/4/4
  * Time: 15:45
  */
+
 namespace app\standard\controller;
+
 use app\admin\controller\Permissions;
 
 class Templatemanage extends Permissions
 {
     public function index()
+    {
+        return $this->fetch();
+    }
+
+    public function add($id = null)
     {
         return $this->fetch();
     }
