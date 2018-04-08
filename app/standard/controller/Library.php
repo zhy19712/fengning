e9c6937458a206f771a1477ad332bfcd7ef630bb
zhifang <373884384@qq.com>
2018-04-08 15:55:56 +0800

    控制点新增编辑

--- app/standard/controller/Library.php
+++ app/standard/controller/Library.php
@@ -9,7 +9,9 @@
 namespace app\standard\controller;
 
 use app\admin\controller\Permissions;
+use app\standard\model\ControlPoint;
 use app\standard\model\MaterialTrackingDivision;
+use think\Request;
 
 /**
  * 标准库
@@ -18,21 +20,54 @@ use app\standard\model\MaterialTrackingDivision;
  */
 class Library extends Permissions
 {
+    protected $controlPointService;
+
+    public function __construct(Request $request = null)
+    {
+        parent::__construct($request);
+        $this->controlPointService = new ControlPoint();
+    }
+
     public function index()
     {
         return $this->fetch();
     }
 
-    public function addcontrollpoint()
+    /**
+     * 新增编辑控制点
+     * @return mixed
+     */
+    public function addcontrollpoint($id = null)
     {
+        if ($this->request->isAjax()) {
+            $mod = input('post.');
+            if (empty($mod['id'])) {
+                $res = $this->controlPointService->allowField(true)->save($mod);
+            } else {
+                $res = $this->controlPointService->allowField(true)->save($mod, ['id' => $mod['id']]);
+            }
+            return $res ? json(['code' => 1]) : json(['code' => -1]);
+        }
+
+        $this->assign('id', $id);
         return $this->fetch();
     }
 
+    /**
+     * 标准库划分树
+     * @param $cat
+     * @return false|static[]
+     * @throws \think\exception\DbException
+     */
     public function GetDivsionTree($cat)
     {
-        return MaterialTrackingDivision::all(['Cat'=>$cat]);
+        return MaterialTrackingDivision::all(['Cat' => $cat]);
     }
 
+    /**
+     * 选择模板
+     * @return mixed
+     */
     public function chosetemplate()
     {
         return $this->fetch();
