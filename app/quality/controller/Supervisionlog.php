0e90169bc467051091ddaad92680c0cf53634ab3
13522390376 <13522390376@163.com>
2018-04-09 11:08:57 +0800

    Merge branch 'master' of http://192.168.1.2:10101/r/fengning
    # Conflicts:
    #	app/archive/controller/Common.php

--- app/quality/controller/Supervisionlog.php
+++ app/quality/controller/Supervisionlog.php
@@ -36,9 +36,9 @@ class Supervisionlog extends Permissions
     public function tree()
     {
         if ($this->request->isAjax()) {
-            //实例化模型ScenePictureModel
+            //实例化模型
             $model = new SupervisionLogModel();
-            //查询fengning_scene_picture现场图片表
+            //查询监理日志表
             $data = $model->getall();
             $res = tree($data);
 
@@ -48,6 +48,327 @@ class Supervisionlog extends Permissions
             }
 
             return json($res);
+        }
+    }
+    /**********************************监理日志************************/
+    /**
+     * 获取一条监理日志信息
+     */
+    public function getindex()
+    {
+        if(request()->isAjax()){
+            $model = new SupervisionLogModel();
+            $param = input('post.');
+            $data = $model->getOne($param['id']);
+            return json(['code'=> 1, 'data' => $data]);
+        }
+        return $this->fetch();
+    }
+
+    /**
+     * 上传监理日志
+     * @return \think\response\Json
+     */
+    public function add()
+    {
+        if(request()->isAjax()){
+            //实例化模型类
+            $model = new SupervisionLogModel();
+            $admin = new Admin();
+            $group = new AdminGroup();
+
+            $param = input('post.');
+
+            //获取当前时间的年月日
+            $year = date("Y");
+            $month = date("m");
+            $day = date("d");
+
+            //根据当前的上传时间获取父级节点id
+            //1.查询当前的年份是否存在,如果年份不存在时，新建一条年份记录
+            $search_info =[
+                "year" => $year,
+                "month" => "",
+                "day" => ""
+            ];
+            $result = $model->getid($search_info);
+            if(!$result['id'])//如果当前的年份不存在就新建当前年的记录
+            {
+                $data = [
+                    "year" => $year,
+                    "name" => $year."年",
+                    'pid' => 1
+                ];
+                //新增一条年份
+                $model -> insertLog($data);
+
+                //新建一条月份记录
+                $search_info1 =[
+                    "year" => $year,
+                    "month" => "",
+                    "day" => ""
+                ];
+                $result1 = $model->getid($search_info1);
+                $data1 = [
+                    "year" => $year,
+                    "month" => $month,
+                    "name" => $month."月",
+                    'pid' => $result1['id']
+                ];
+                //新增一条月份记录
+                $model -> insertLog($data1);
+
+                //新建一条日份记录
+                $search_info2 =[
+                    "year" => $year,
+                    "month" => $month,
+                    "day" => ""
+                ];
+                $result2 = $model->getid($search_info2);
+                $admin_id = Session::get('current_id');
+                $admininfo = $admin->getadmininfo($admin_id);
+                $group = $group->getOne($admininfo["admin_group_id"]);
+                $data2 = [
+                    "year" => $year,
+                    "month" => $month,
+                    "day" => $day,
+                    "name" => $day."日",
+                    "pid" => $result2['id'],
+                    "filename" => date("YmdHis"),//默认上传的文件名为日期
+                    "date" => date("Y-m-d H:i:s"),
+                    "owner" => Session::get('current_name'),
+                    "company" => $group["name"],//单位
+                    "admin_group_id" => $admininfo["admin_group_id"],
+                    "path" => $param['path']//路径
+                ];
+                $flag = $model->insertLog($data2);
+                return json($flag);
+
+
+            }else{
+                //2.查询当前的月份是否存在,如果月份不存在时，新建一条月份记录
+                $search_info =[
+                    "year" => $year,
+                    "month" => $month,
+                    "day" => ""
+                ];
+                $result = $model->getid($search_info);
+                if(!$result['id'])//如果当前的月份不存在就新建当前月的记录
+                {
+                    $search_info1 =[
+                        "year" => $year,
+                        "month" => "",
+                        "day" => ""
+                    ];
+                    $result1 = $model->getid($search_info1);
+
+                    $data1 = [
+                        "year" => $year,
+                        "month" => $month,
+                        "name" => $month."月",
+                        'pid' => $result1['id']
+                    ];
+                    //新增一条月份
+                    $model -> insertLog($data1);
+                    //新建一条日份记录
+                    $search_info2 =[
+                        "year" => $year,
+                        "month" => $month,
+                        "day" => ""
+                    ];
+                    $result2 = $model->getid($search_info2);
+                    $admin_id = Session::get('current_id');
+                    $admininfo = $admin->getadmininfo($admin_id);
+                    $group = $group->getOne($admininfo["admin_group_id"]);
+                    $data2 = [
+                        "year" => $year,
+                        "month" => $month,
+                        "day" => $day,
+                        "name" => $day."日",
+                        "pid" => $result2['id'],
+                        "filename" => date("YmdHis"),//默认上传的文件名为日期
+                        "date" => date("Y-m-d H:i:s"),
+                        "owner" => Session::get('current_name'),
+                        "company" => $group["name"],//单位
+                        "admin_group_id" => $admininfo["admin_group_id"],
+                        "path" => $param['path']//路径
+                    ];
+                    $flag = $model->insertLog($data2);
+                    return json($flag);
+
+                }else{
+                    //3.如果当前的年份、月份都存在时，新增完整的一条现场图片信息
+                    //查询当前登录的用户所属的组织机构名
+                    $search_info =[
+                        "year" => $year,
+                        "month" => $month,
+                        "day" => ""
+                    ];
+                    $result = $model->getid($search_info);
+                    $admin_id = Session::get('current_id');
+                    $admininfo = $admin->getadmininfo($admin_id);
+                    $group = $group->getOne($admininfo["admin_group_id"]);
+                    $data = [
+                        "year" => $year,
+                        "month" => $month,
+                        "day" => $day,
+                        "name" => $day."日",
+                        "pid" => $result['id'],
+                        "filename" => date("YmdHis"),//默认上传的文件名为日期
+                        "date" => date("Y-m-d H:i:s"),
+                        "owner" => Session::get('current_name'),
+                        "company" => $group["name"],//单位
+                        "admin_group_id" => $admininfo["admin_group_id"],
+                        "path" => $param['path']//路径
+                    ];
+                    $flag = $model->insertLog($data);
+                    return json($flag);
+                }
+            }
+        }
+    }
+
+    /**
+     * 编辑一条监理日志信息
+     */
+    public function edit()
+    {
+        if(request()->isAjax()){
+            $model = new SupervisionLogModel();
+            $param = input('post.');
+            $data = [
+                'id' => $param['id'],//监理日志自增id
+                'filename' => $param['filename']//上传文件名
+            ];
+            $flag = $model->editLog($data);
+            return json($flag);
+
+        }
+    }
+
+    /**
+     * 下载一条监理日志信息
+     * @return \think\response\Json
+     */
+    public function download()
+    {
+        if(request()->isAjax()){
+            $id = input('param.id');
+            $model = new SupervisionLogModel();
+            $param = $model->getOne($id);
+            if(!$param['path'] || !file_exists("." .$param['path'])){
+                return json(['code' => '-1','msg' => '文件不存在']);
+            }
+            return json(['code' => 1]);
+        }
+        $id = input('param.id');
+        $model = new SupervisionLogModel();
+        $param = $model->getOne($id);
+
+        $filePath = '.' . $param['path'];
+        $fileName = $param['filename'];
+        $file = fopen($filePath, "r"); //   打开文件
+        //输入文件标签
+        $fileName = iconv("utf-8","gb2312",$fileName);
+        Header("Content-type:application/octet-stream ");
+        Header("Accept-Ranges:bytes ");
+        Header("Accept-Length:   " . filesize($filePath));
+        Header("Content-Disposition:   attachment;   filename= " . $fileName);
+        //   输出文件内容
+        echo fread($file, filesize($filePath));
+        fclose($file);
+        exit;
+
+    }
+
+    /**
+     * 删除一条监理日志信息
+     */
+    public function del()
+    {
+
+        if (request()->isAjax()) {
+
+            $id = input('post.id');//要删除的监理日志id
+
+            //实例化model类型
+            $model = new SupervisionLogModel();
+            //首先判断一下删除的只一天所属的月份下是否有其他日子
+            $data_info = $model->getOne($id);
+
+            $day_count = $model->getcount($data_info['pid']);
+
+            $data_month = $model->getOne($data_info["pid"]);
+
+            //如果一个月份下只有一条的话就删除这个月份
+            if($day_count < 2)
+            {
+                $model -> delLog($data_info['pid']);
+
+            }
+
+            //判断年份下只有一条的话就删除这个年份
+            $year_count = $model->getcount($data_month['pid']);
+            if($year_count < 1)
+            {
+                //如果一个年份下只有一条的话就删除这个年份
+                $model -> delLog($data_month['pid']);
+            }
+
+            //最后删除这条现场图片信息
+            $path = "." .$data_info['path'];
+            $pdf_path = './uploads/temp/' . basename($path) . '.pdf';
+            if(file_exists($path)){
+                unlink($path); //删除上传的图片或文件
+            }
+            if(file_exists($pdf_path)){
+                unlink($pdf_path); //删除生成的预览pdf
+            }
+
+            //最后删除这一条记录信息
+            $flag = $model->delLog($id);
+            return $flag;
+
+        }
+    }
+
+    /**
+     * 预览一条监理日志信息
+     * @return \think\response\Json
+     */
+    public function preview()
+    {
+        $model = new SupervisionLogModel();
+        if(request()->isAjax()) {
+            $param = input('post.');
+            $code = 1;
+            $msg = '预览成功';
+            $data = $model->getOne($param['id']);
+            if(!$data['path'] || !file_exists("." .$data['path'])){
+                return json(['code' => '-1','msg' => '文件不存在']);
+            }
+            $path = $data['path'];
+            $extension = strtolower(get_extension(substr($path,1)));
+            $pdf_path = './uploads/temp/' . basename($path) . '.pdf';
+            if(!file_exists($pdf_path)){
+                if($extension === 'doc' || $extension === 'docx' || $extension === 'txt'){
+                    doc_to_pdf($path);
+                }else if($extension === 'xls' || $extension === 'xlsx'){
+                    excel_to_pdf($path);
+                }else if($extension === 'ppt' || $extension === 'pptx'){
+                    ppt_to_pdf($path);
+                }else if($extension === 'pdf'){
+                    $pdf_path = $path;
+                }else if($extension === "jpg" || $extension === "png" || $extension === "jpeg"){
+                    $pdf_path = $path;
+                }else {
+                    $code = 0;
+                    $msg = '不支持的文件格式';
+                }
+                return json(['code' => $code, 'path' => substr($pdf_path,1), 'msg' => $msg]);
+            }else{
+                return json(['code' => $code,  'path' => substr($pdf_path,1), 'msg' => $msg]);
+            }
         }
     }
 }
