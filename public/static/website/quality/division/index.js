c3ce8e7d6bb14b69d9d1b130de97dc2a2b7ff909
wyang <wyang1117@msn.com>
2018-04-11 17:56:57 +0800

    解决新增段号失败,点击节点刷table报错的问题

--- public/static/website/quality/division/index.js
+++ public/static/website/quality/division/index.js
@@ -5,6 +5,7 @@ var form = layui.form;
 $.ztree({
     //点击节点
     zTreeOnClick:function (event, treeId, treeNode){
+        tableInfo();
         $.clicknode({
             tableItem:tableItem,
             treeNode:treeNode,
@@ -206,51 +207,55 @@ $('#save').click(function () {
 });
 
 //table数据
-$.datatable({
-    ajax:{
-        'url':'/quality/common/datatablesPre?tableName=quality_unit'
-    },
-    dom: 'lf<".current-path"<"#add.add layui-btn layui-btn-normal layui-btn-sm">>tipr',
-    columns:[
-        {
-            name: "serial_number"
+function tableInfo() {
+    $.datatable({
+        tableId:'tableItem',
+        ajax:{
+            'url':'/quality/common/datatablesPre?tableName=quality_unit'
         },
-        {
-            name: "site"
-        },
-        {
-            name: "coding"
-        },
-        {
-            name: "hinge"
-        },
-        {
-            name: "pile_number"
-        },
-        {
-            name: "start_date"
-        },
-        {
-            name: "completion_date"
-        },
-        {
-            name: "id"
-        }
-    ],
-    columnDefs:[
-        {
-            "searchable": false,
-            "orderable": false,
-            "targets": [7],
-            "render" :  function(data,type,row) {
-                var html = "<i class='fa fa-pencil' uid="+ data +" title='编辑' onclick='edit(this)'></i>" ;
-                html += "<i class='fa fa-trash' uid="+ data +" title='删除' onclick='del(this)'></i>" ;
-                html += "<i class='fa fa-qrcode' uid="+ data +" title='二维码' onclick='qrcode(this)'></i>" ;
-                return html;
+        dom: 'lf<".current-path"<"#add.add layui-btn layui-btn-normal layui-btn-sm">>tipr',
+        columns:[
+            {
+                name: "serial_number"
+            },
+            {
+                name: "site"
+            },
+            {
+                name: "coding"
+            },
+            {
+                name: "hinge"
+            },
+            {
+                name: "pile_number"
+            },
+            {
+                name: "start_date"
+            },
+            {
+                name: "completion_date"
+            },
+            {
+                name: "id"
             }
-        }
-    ],
-});
+        ],
+        columnDefs:[
+            {
+                "searchable": false,
+                "orderable": false,
+                "targets": [7],
+                "render" :  function(data,type,row) {
+                    var html = "<i class='fa fa-pencil' uid="+ data +" title='编辑' onclick='edit(this)'></i>" ;
+                    html += "<i class='fa fa-trash' uid="+ data +" title='删除' onclick='del(this)'></i>" ;
+                    html += "<i class='fa fa-qrcode' uid="+ data +" title='二维码' onclick='qrcode(this)'></i>" ;
+                    return html;
+                }
+            }
+        ],
+    });
+}
+tableInfo();
 $('#add').html('新增');
 
 $('#add').click(function () {
@@ -300,6 +305,7 @@ layui.use('laydate', function(){
 });
 
 $('.maBasesBtn').click(function () {
+    $('.tbcontainer:last-child').remove();
     layer.open({
         title:'添加施工依据',
         id:'100',
