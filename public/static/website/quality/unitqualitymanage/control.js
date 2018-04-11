c0d2fb49e40520c15cccaf2eca854e7cc587b93a
wyang <wyang1117@msn.com>
2018-04-11 18:25:51 +0800

    添加节点树

---
+++ public/static/website/quality/unitqualitymanage/control.js
@@ -0,0 +1,12 @@
+$.ztree({
+    //点击节点
+    zTreeOnClick:function (event, treeId, treeNode){
+        tableInfo();
+        $.clicknode({
+            tableItem:tableItem,
+            treeNode:treeNode,
+            tablePath:'/quality/common/datatablesPre?tableName=quality_unit',
+            isLoadPath:false
+        });
+    }
+});
