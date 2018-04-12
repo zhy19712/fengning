53b6ddbdd2b012feb3cebb2979025ccd114519ab
zhifang <373884384@qq.com>
2018-04-12 10:37:47 +0800

    控制点获取方式

--- app/quality/controller/Element.php
+++ app/quality/controller/Element.php
@@ -10,6 +10,8 @@ namespace app\quality\controller;
 
 use app\admin\controller\Permissions;
 use app\quality\model\DivisionUnitModel;
+use app\standard\model\ControlPoint;
+use think\Db;
 
 class Element extends Permissions
 {
@@ -54,5 +56,24 @@ class Element extends Permissions
     public function getDivisionUnitTree($id)
     {
         return json(DivisionUnitModel::all(['division_id' => $id]));
+    }
+
+    /**
+     * 获取控制点
+     * @param $id 工序Id
+     */
+    public function getControlPointsByProcedureId($id)
+    {
+        return json(ControlPoint::all(['procedureid' => $id]));
+    }
+
+    /**
+     * 获取控制点
+     * @param $id 检验批Id
+     */
+    public function getCPByDivisionId($id)
+    {
+        $ids[] = Db::name('quality_division')->where(['pid' => $id])->field('id');
+        return json(Db::name('controlpoint')->whereIn('procedureid', $ids));
     }
 }
