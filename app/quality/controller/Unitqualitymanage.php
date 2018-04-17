dcb21e2878af290c125837ec1dfa2be38a6d0eec
huwtao <hutaosir@163.com>
2018-04-17 09:25:55 +0800

    norm commit

--- app/quality/controller/Unitqualitymanage.php
+++ app/quality/controller/Unitqualitymanage.php
@@ -12,8 +12,9 @@ namespace app\quality\controller;
 use app\admin\controller\Permissions;
 use app\quality\model\DivisionModel;
 use app\quality\model\UnitqualitymanageModel;
-use app\standard\model\ControlPoint;
 use think\Db;
+use think\Loader;
+use think\Session;
 
 // 单位质量管理
 
@@ -59,10 +60,99 @@ class Unitqualitymanage extends Permissions
         return json(['code' => 1,'data' => $data,'msg' => '工序']);
     }
 
-    //TODO 导出二维码 预留接口
-    public function exportCode()
+    /**
+     * 批量下载 二维码
+     * @return \think\response\Json
+     * @author hutao
+     */
+    public function indexssss()
     {
-
+        // 前台 传递 要下载 哪个节点 下的所有 二维码 add_id
+        $id = $this->request->has('add_id') ? $this->request->param('add_id', 0, 'intval') : 1;
+        if($id == 0){
+            return json(['code' => '-1','msg' => '请选择工程划分节点']);
+        }
+//        if($this->request->isAjax()){
+            $attachment_id = [];
+            // 获取 工程划分 下 所有的 控制点
+            $control = Db::name('quality_division_controlpoint_relation')->alias('d')
+                ->join('materialtrackingdivision m','d.ma_division_id = m.id','left')
+                ->join('controlpoint c','d.control_id = c.id','left')
+                ->where(['d.division_id'=>$id,'d.type'=>0])->column('d.control_id,m.name as m_name,c.name as c_name');
+            if(empty($control)){
+                return json(['code' => '-1','msg' => '没有数据']);
+            }
+            $qrcode_bas_path = ROOT_PATH . 'public' .DS . 'uploads' . DS . 'quality' . DS . 'export-code';//文件路径
+            if(!is_dir($qrcode_bas_path)){
+                mkdir($qrcode_bas_path, 0777, true);
+            }
+            foreach ($control as $k=>$v){
+                $data['module'] = 'quality';
+                // 图片名称 是 工序名称-控制点编号-控制点名称.png
+                $data['filename'] = $v['m_name'] . '-' . $v['control_id'] . '-' . $v['c_name'] . '.png';//文件名
+                $data['filepath'] = DS . 'uploads' . DS . 'quality' . DS . 'export-code' . DS . $data['filename'];//文件路径
+                // 生成二维码图片
+                $png_name = iconv("utf-8","gb2312",$data['filename']); // 图片名称
+                $png_path = $qrcode_bas_path . DS . $png_name;
+                Loader::import('phpqrcode\phpqrcode', EXTEND_PATH);
+                $text = url('quality/Unitqualitymanage/fileDownload/'.$v['control_id']);
+                if(!file_exists($png_path)) {
+                    \QRcode::png($text,$png_path,'L',5,2);
+                }
+                $data['fileext'] = 'png';//文件后缀
+                $data['filesize'] = '';//无法获取文件大小
+                $data['create_time'] = time();//时间
+                $data['uploadip'] = $this->request->ip();//IP
+                $data['user_id'] = Session::has('admin') ? Session::get('admin') : 0;
+                if($data['module'] == 'admin') {
+                    //通过后台上传的文件直接审核通过
+                    $data['status'] = 1;
+                    $data['admin_id'] = $data['user_id'];
+                    $data['audit_time'] = time();
+                }
+                $data['use'] = 'exportCode';//用处
+                // 首先判断是否已经 生成过 二维码图片
+                $insert_id = Db::name('attachment')->where(['module'=>$data['module'],'filename'=>$data['filename']])->value('id');
+                if(empty($insert_id)){
+                    $insert_id = Db::name('attachment')->insertGetId($data);
+                }
+                $attachment_id[] = $insert_id;
+            }
+            $datalist = Db::name("attachment")->whereIn("id", $attachment_id)->column('filepath');
+            $zip = new \ZipArchive;
+            // 压缩文件名
+            $d_name = Db::name('quality_division')->where('id',$id)->value('d_name');
+//            $new_png_name = iconv("utf-8","gb2312",$d_name);
+            $zipName = ROOT_PATH . 'public' .DS .'uploads/quality/export-code/'.'aa'.'.zip';
+            //新建zip压缩包
+            if ($zip->open($zipName, \ZIPARCHIVE::CREATE) === TRUE) {
+                foreach($datalist as $key=>$val){
+                    $new_val = ROOT_PATH . 'public' . $val;
+                    dump($new_val);
+                    if(file_exists($new_val)){
+                        halt($new_val);
+                        //addFile函数首个参数如果带有路径，则压缩的文件里包含的是带有路径的文件压缩
+                        //若不希望带有路径，则需要该函数的第二个参数
+                        $zip->addFile($new_val, basename($new_val));//第二个参数是放在压缩包中的文件名称，如果文件可能会有重复，就需要注意一下
+                    }
+                }
+            }
+            //打包zip
+            $zip->close();
+            if(!file_exists($zipName)){
+                exit("无法找到文件"); //即使创建，仍有可能失败
+            }
+            //如果不要下载，下面这段删掉即可，如需返回压缩包下载链接，只需 return $zipName;
+            header("Cache-Control: public");
+            header("Content-Description: File Transfer");
+            header('Content-disposition: attachment; filename='.basename($zipName)); //文件名
+            header("Content-Type: application/zip"); //zip格式的
+            header("Content-Transfer-Encoding: binary"); //告诉浏览器，这是二进制文件
+            header('Content-Length: '. filesize($zipName)); //告诉浏览器，文件大小
+            @readfile($zipName);
+            //最后删除指定改的下载包，防止文件重复
+            unlink($zipName);
+//        }
     }
 
 
