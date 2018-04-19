5b3d3337ae19a05513ddd84987854905c8ea33af
13522390376 <13522390376@163.com>
2018-04-19 08:52:58 +0800

    quality

--- app/statistics/controller/Qualityanalysis.php
+++ app/statistics/controller/Qualityanalysis.php
@@ -12,6 +12,7 @@
  */
 namespace app\statistics\controller;
 use app\admin\controller\Permissions;
+use app\quality\model\DivisionModel;//工程划分
 use \think\Session;
 use think\exception\PDOException;
 use think\Loader;
@@ -26,5 +27,53 @@ class Qualityanalysis extends Permissions
     public function index()
     {
         return $this->fetch();
+    }
+
+    public function getIndex()
+    {
+        //获取最小的时间
+        $date = Db::name("quality_form_info")->where("create_time > 0")->min("create_time");
+        //定义一个空的数组
+        $data = array();
+        $timeline = array();
+        $excellentMonth = array();
+        $rate = array();
+        $StartMonth = date("Y-m-d",$date); //开始日期
+        $EndMonth = date("Y-m-d"); //结束日期
+        $ToStartMonth = strtotime( $StartMonth ); //转换一下
+        $ToEndMonth   = strtotime( $EndMonth ); //一样转换一下
+        $i            = false; //开始标示
+        while( $ToStartMonth < $ToEndMonth ){
+            $NewMonth = !$i ? date('Y-m', strtotime('+0 Month', $ToStartMonth)) : date('Y-m', strtotime('+1 Month', $ToStartMonth));
+            $ToStartMonth = strtotime( $NewMonth );
+            $i = true;
+            $timeline[] = $NewMonth;//时间
+
+            $excellentMonth[] = 50;//优良率
+
+            $rate[] = array("excellent_number"=>70,"qualified_number"=>30);//优良单元数量，合格单元数量
+        }
+
+
+        $data = ["timeline"=>$timeline,"excellentMonth"=>$excellentMonth,"rate"=>$rate];
+
+        return json(["code"=>1,"data"=>$data]);
+
+
+    }
+
+    public function section()
+    {
+
+    }
+
+    public function getForm()
+    {
+        $data = array();
+        $info = Db::name("quality_form_info")->field("form_data")->select();
+        foreach ($info as $key=>$val) {
+           $data[$key]= unserialize($val["form_data"]);
+        }
+        halt($data);
     }
 }
