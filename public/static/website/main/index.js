c35f96db0494cc244d0f0a963cbd06d8415167d3
wyang <wyang1117@msn.com>
2018-04-25 20:33:29 +0800

    完成快照及标注回显功能对接

--- public/static/website/main/index.js
+++ public/static/website/main/index.js
@@ -35,7 +35,7 @@ window.snapshotSwiper = new Swiper ('#snapshot', {
     spaceBetween: 30,
 });
 
-//添加标注/快照
+//添加快照
 window.addSnapshot = function (base64RenData) {
     var img =
         '<div class="swiper-slide swiperSlide">' +
