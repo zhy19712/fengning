dc154f1d081dbdaba56cf30769d6ff8d13ea6ab0
zhifang <373884384@qq.com>
2018-04-25 18:04:20 +0800

    审批历史信息

--- app/approve/controller/Approve.php
+++ app/approve/controller/Approve.php
@@ -62,6 +62,8 @@ class Approve extends Permissions
                 return json(['code' => -1]);
             }
         }
+        $this->assign('dataId', $par['dataId']);
+        $this->assign('dataType', $par['dataType']);
         $this->assign('ApproveInfo', json_encode($this->approveService->getApproveInfo($par['dataId'], new $par['dataType'])));
         return $this->fetch();
     }
