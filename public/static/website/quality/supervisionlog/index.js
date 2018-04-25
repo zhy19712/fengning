65078941ec84f82c60c1678f4a2e3b26addc7ef8
dingzicha <15893001150@163.com>
2018-04-25 19:09:08 +0800

    改动

--- public/static/website/quality/supervisionlog/index.js
+++ public/static/website/quality/supervisionlog/index.js
@@ -299,19 +299,22 @@ function conPicshow(id){
 }
 //设置位置
 function conPosition(id) {
-  window.open("../scenepicture/PositionSet?id=" + id,"_blank","height=600, width=900, top=200,left=400, toolbar=no, menubar=no, scrollbars=no, resizable=no,location=no,status=no",false );
+  // window.open("../scenepicture/PositionSet?id=" + id,"_blank","height=600, width=900, top=200,left=400, toolbar=no, menubar=no, scrollbars=no, resizable=no,location=no,status=no",false );
 
-  // layer.open({
-  //       type: 2,
-  //       shadeClose: true,
-  //       title: "空间位置设置",
-  //       area: ["900px", "600px"],
-  //       content: "../scenepicture/PositionSet?id=" + id,
-  //       success: function(layero, index){
-  //           var body = layer.getChildFrame('body', index);
-  //           body.find('input').val(positionUrl)
-  //       }
-  //   });
+  layer.open({
+        type: 2,
+        shadeClose: true,
+        title: "空间位置设置",
+        area: ["900px", "600px"],
+        content: "../scenepicture/PositionSet?id=" + id,
+        success: function(layero, index){
+            var body = layer.getChildFrame('body', index);
+            body.find('input').val(positionUrl)
+        },
+        end:function () {
+          refreshTable();
+        }
+    });
 }
 //datatables表格
 var tableItem = $('#tableItem').DataTable( {
