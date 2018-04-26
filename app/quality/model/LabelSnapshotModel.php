0591dd7b84a2cf6fe82f9909fafbf00b3143fd22
huwtao <hutaosir@163.com>
2018-04-26 11:14:45 +0800

    picture-model

--- app/quality/model/LabelSnapshotModel.php
+++ app/quality/model/LabelSnapshotModel.php
@@ -63,11 +63,4 @@ class LabelSnapshotModel extends Model
         return ['code'=>1,'data'=>$data,'msg'=>'图片的base64值'];
     }
 
-    public function getNameAndTime($id)
-    {
-        // 类型 type 1标注2快照
-        $data = $this->where(['type'=>1,'id'=>$id])->column('user_name,FROM_UNIXTIME(create_time) as create_time');
-        return ['code'=>1,'data'=>$data,'msg'=>'创建人和创建时间'];
-    }
-
 }
