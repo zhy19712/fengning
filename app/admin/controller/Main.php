9f4aab5983a2affa4290da36d8ed2c83a745a26b
13522390376 <13522390376@163.com>
2018-05-03 16:03:18 +0800

    filemanagement

--- app/admin/controller/Main.php
+++ app/admin/controller/Main.php
@@ -267,8 +267,27 @@ class Main extends Permissions
                 return json(['code' => -1,'msg' => $validate->getError()]);
             }
             $pic = new LabelSnapshotModel();
+
             $flag = $pic->getLabelSnapshotTb($param['type'],$param['picture_id']);
-            return json($flag);
+
+            $file = ROOT_PATH . 'public' .DS . 'uploads' . DS . 'quality' . DS . 'snapshot' . DS;//文件路径
+
+            if(!file_exists($file))
+            {
+                //检查是否有该文件夹，如果没有就创建，并给予最高权限
+                mkdir($file, 0700);
+            }
+
+           foreach ($flag as $key=>$val)
+           {
+               $new_file = $file.md5(rand(1,9999)).".jpg";
+
+               file_put_contents($new_file, base64_decode($val["compress_base64"]));
+
+               $flag[$key]["url"] = $new_file;
+           }
+
+           return json(['code'=>1,"data"=>$flag,"msg"=>"图片的base64值"]);
         }
     }
 
