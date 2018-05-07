f0134e9a5f58009b779308dea979442b28e0edde
wyang <wyang1117@msn.com>
2018-05-07 16:57:51 +0800

    注销基本信息模板

--- public/static/website/main/manage.js
+++ public/static/website/main/manage.js
@@ -1,7 +1,3 @@
-layui.use('element', function(){
-    var element = layui.element;
-});
-
 function easyUiPanelToggle() {
     var number = $("#easyuiLayout").layout("panel", "east")[0].clientWidth;
     if(number<=0){
