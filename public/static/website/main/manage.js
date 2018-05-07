aa5b44fcf593789b6ea63aa47a03bd68e56d6502
wyang <wyang1117@msn.com>
2018-05-07 16:49:17 +0800

    添加基本信息模板

--- public/static/website/main/manage.js
+++ public/static/website/main/manage.js
@@ -1,3 +1,7 @@
+layui.use('element', function(){
+    var element = layui.element;
+});
+
 function easyUiPanelToggle() {
     var number = $("#easyuiLayout").layout("panel", "east")[0].clientWidth;
     if(number<=0){
